07233e392d2d69444941d21c9fb2f1a8754261cc
vladsoanca <vladsoanca@yahoo.com>
2019-05-23 20:45:13 +0300

    Update

---
+++ src/com/company/Main.java
@@ -0,0 +1,50 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+    //write your code here
+        char myChar = 'a';
+        byte myByte = 123;
+        short myShort = 12312;
+        int myInt = 1;
+        long myLong = 123132132132l;
+        float myFloat= 12312;
+        String myString = "MyName" ;
+        int result = 5*5;
+        System.out.println("result is: "+result);
+        /*
+      int a=5;
+
+
+
+
+        System.out.println(a);
+        System.out.println(++a);
+        System.out.println(a);
+        */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
