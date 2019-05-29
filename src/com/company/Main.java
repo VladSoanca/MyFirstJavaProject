f147c19f6bdaaf2c70590b8639bd4182a5fed632
vladsoanca <vladsoanca@yahoo.com>
2019-05-29 11:06:07 +0300

    Tema

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,32 +3,25 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-    //write your code here
-        char myChar = 'a';
-        byte myByte = 123;
-        short myShort = 12312;
-        int myInt = 1;
-        long myLong = 123132132132l;
-        float myFloat= 12312;
-        String myString = "MyName" ;
-        int result = 5*5;
-        System.out.println("result is: "+result);
-        /*
-      int a=5;
 
+        System.out.println("Hello");
+        System.out.println("Vlad");
 
+        int variableA ;
+        variableA=4+5;
+        System.out.println(variableA);
 
+        double variableB,variableC=10.2,variableD=2.3;
+        variableB=variableC/variableD;
+        System.out.println(variableB);
 
+        System.out.println("Rezultatul operatiei -5+8*6 este "+(-5+(8*6)));
 
-        System.out.println(a);
-        System.out.println(++a);
-        System.out.println(a);
-        */
+        System.out.println("Rezultatul operatiei (55+9)%9 este "+((55+9)%9));
 
+        System.out.println("Rezultatu operatiei 20+(-3)*5/8 este "+(20+(-3*5/8)));
 
-
-
-
+        System.out.println("Rezultatu operatiei 5+15/3*2-8%3 este "+(5+(15/3*2-8%3)));
 
 
 
